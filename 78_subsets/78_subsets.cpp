7197e9a9e8dc99e8caf85d8c03f285d56fadcfa2
change <changyy66@163.com>
2019-10-03 12:51:25 +0800

    add

--- 78_subsets/78_subsets.cpp
+++ 78_subsets/78_subsets.cpp
@@ -2,10 +2,41 @@
 //
 
 #include "stdafx.h"
+#include <vector>
+#include <iostream>
+using namespace std;
 
+void next(vector<int>& nums, vector<vector<int>>& res, vector<int>& v, int start)
+{
+	res.push_back(v);
+	for (int i=start;i<nums.size();i++)
+	{
+		v.push_back(nums[i]);
+		next(nums, res, v, i + 1);
+		v.pop_back();
+	}
+}
+
+vector<vector<int>> subsets(vector<int>& nums) {
+	vector<vector<int>> res;
+	vector<int> v;
+	next(nums, res, v, 0);
+	return res;
+}
 
 int main()
 {
+	vector<int> nums = { 1};
+	vector<vector<int>> res = subsets(nums);
+	for each (auto var in res)
+	{
+		cout << "[";
+		for each (auto item in var)
+		{
+			cout << item << " ";
+		}
+		cout << "]\r\n";
+	}
     return 0;
 }
 
